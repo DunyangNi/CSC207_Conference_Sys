e6569a2619e1c4e9f710a4026693141279f9b5a0
Andrew Ho <andrewjiameng.ho@mail.utoronto.ca>
2020-11-01 10:07:18 -0500

    Split into packages Added package statements Added appropriate import statements

--- phase1/src/Attendee.java
+++
@@ -1,6 +0,0 @@
-public class Attendee extends Account {
-
-    public Attendee(String username, String password, String firstName, String lastName) {
-        super(username, password, firstName, lastName);
-    }
-}
