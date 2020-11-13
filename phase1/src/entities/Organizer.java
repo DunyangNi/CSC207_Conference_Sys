7f6d14be7271a7af653f75059421e8bc3dbfe865
fanhaoli <haolin.fan@mail.utoronto.ca>
2020-11-13 00:05:56 -0500

    Deleted Speaker.speakingTalks and Organizer.organizerEvents. Will cause some usage issues in EventManager.

--- phase1/src/entities/Organizer.java
+++ phase1/src/entities/Organizer.java
@@ -4,17 +4,7 @@ import java.util.ArrayList;
 
 public class Organizer extends Account {
 
-    private ArrayList<Integer> organizerEvents = new ArrayList<>();
-
     public Organizer(String username, String password, String firstName, String lastName) {
         super(username, password, firstName, lastName);
-    }
-
-    public ArrayList<Integer> getOrganizerEvents() {
-        return organizerEvents;
-    }
-
-    public void setOrganizerEvents(ArrayList<Integer> organizerEvents) {
-        this.organizerEvents = organizerEvents;
     }
 }
--- phase1/src/entities/Speaker.java
+++ phase1/src/entities/Speaker.java
@@ -4,17 +4,7 @@ import java.util.ArrayList;
 
 public class Speaker extends Account {
 
-    private ArrayList<Integer> speakerTalks = new ArrayList<>();
-
     public Speaker(String username, String password, String firstName, String lastName) {
         super(username, password, firstName, lastName);
-    }
-
-    public ArrayList<Integer> getSpeakerTalks() {
-        return speakerTalks;
-    }
-
-    public void setSpeakerTalks(ArrayList<Integer> speakerTalks) {
-        this.speakerTalks = speakerTalks;
     }
 }
