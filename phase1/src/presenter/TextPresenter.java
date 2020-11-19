869499f78fc961e9f2f3fbfd9a5d776d8e91e1aa
fanhaoli <haolin.fan@mail.utoronto.ca>
2020-11-19 16:41:43 -0500

    Fixed order of organizer commands in TextPresenter and OrganizerController.

--- phase1/src/presenter/TextPresenter.java
+++ phase1/src/presenter/TextPresenter.java
@@ -55,29 +55,28 @@ public class TextPresenter extends Presenter{
         System.out.println("[ACCOUNT]");
         System.out.println("00  = Logout and exit program");
         System.out.println("0  = Logout");
-        System.out.println("2  = Register a new speaker account");
-        System.out.println("17 = [FOR TESTING PURPOSES] Delete an account");
-        System.out.println("16 = View list of all accounts");
+        System.out.println("1  = Register a new speaker account");
+        System.out.println("2 = View list of all accounts");
 
         System.out.println("\n[CONTACTS]");
-        System.out.println("11 = Add a contact");
-        System.out.println("12 = Remove a contact");
-        System.out.println("13 = View list of all contacts");
+        System.out.println("3 = Add a contact");
+        System.out.println("4 = Remove a contact");
+        System.out.println("5 = View list of all contacts");
 
         System.out.println("\n[CONVERSATIONS]");
-        System.out.println("7  = Message a speaker");
-        System.out.println("9  = Message an attendee");
-        System.out.println("6  = Message all speakers");
-        System.out.println("8  = Message all attendees");
-        System.out.println("14 = View your conversations");
+        System.out.println("6  = Message a speaker");
+        System.out.println("7  = Message an attendee");
+        System.out.println("8  = Message all speakers");
+        System.out.println("9  = Message all attendees");
+        System.out.println("10 = View your conversations");
 
         System.out.println("\n[EVENTS]");
-        System.out.println("1  = Register a new event room");
-        System.out.println("15 = View list of all rooms");
-        System.out.println("3  = Register a new event");
-        System.out.println("4  = Cancel an event");
-        System.out.println("5  = Reschedule an event");
-        System.out.println("10 = View talk schedule");
+        System.out.println("11 = Register a new event room");
+        System.out.println("12 = View list of all rooms");
+        System.out.println("13  = Register a new event");
+        System.out.println("14  = Cancel an event");
+        System.out.println("15  = Reschedule an event");
+        System.out.println("16 = View talk schedule");
         System.out.println("==========================");
     }
 
