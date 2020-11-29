c0a415e0c0f75814d31f0c86393e6055178cd37f
Jerry Bao <bao.jerry.can@gmail.com>
2020-11-29 14:28:42 -0500

    hi

--- phase2/src/entities/Request.java
+++ phase2/src/entities/Request.java
@@ -1,4 +1,32 @@
 package entities;
-
+import java.util.*;
 public class Request {
+    private Calendar timeOfRequest;
+    private String senderUsername;
+    private String request;
+    private Boolean resolved = false;
+
+    public Request(Calendar timeOfRequest, String senderUsername, String request) {
+        this.timeOfRequest = timeOfRequest;
+        this.senderUsername = senderUsername;
+        this.request = request;
+    }
+
+    public Calendar getTimeOfRequest(){
+        return this.timeOfRequest;
+    }
+    public String getSenderUsername(){
+        return this.senderUsername;
+    }
+    public String getRequest(){
+        return this.request;
+    }
+    public Boolean getResolutionSatus(){
+        return this.resolved;
+    }
+
+    public void resolveRequest(){
+        this.resolved = true;
+    }
+
 }
