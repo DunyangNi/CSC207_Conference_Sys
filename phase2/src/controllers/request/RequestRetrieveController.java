592b519a102c7c644513b2279846ba6284ba774d
Jerry Bao <bao.jerry.can@gmail.com>
2020-12-08 19:56:58 -0500

    hi

--- phase2/src/controllers/request/RequestRetrieveController.java
+++ phase2/src/controllers/request/RequestRetrieveController.java
@@ -1,4 +1,17 @@
 package controllers.request;
 
+import exceptions.not_found.ObjectNotFoundException;
+import gateways.DataManager;
+import use_cases.*;
+
 public class RequestRetrieveController {
+    public final RequestManager rm;
+    //TODO: modify gateways to accomodate requestmanager
+    public RequestRetrieveController(DataManager dm) {
+        this.rm = dm.getRequestManager();
+    }
+
+    public void resolveRequest(Integer requestID) throws ObjectNotFoundException {
+        this.rm.resolveRequest(requestID);
+    }
 }
--- phase2/src/controllers/request/RequestSendController.java
+++ phase2/src/controllers/request/RequestSendController.java
@@ -1,4 +1,17 @@
 package controllers.request;
 
+import gateways.DataManager;
+import use_cases.*;
+
 public class RequestSendController {
+    public final RequestManager rm;
+    //TODO: modify gateways to accomodate requestmanager
+    public RequestSendController(DataManager dm) {
+        this.rm = dm.getRequestManager();
+    }
+
+
+    public void sendRequest(String senderUsername, String requestSubjectLine, String request) {
+        this.rm.sendRequest(senderUsername, requestSubjectLine, request);
+    }
 }
