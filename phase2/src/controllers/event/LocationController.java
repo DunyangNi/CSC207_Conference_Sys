16d298d0196a2679185c17fa77644b88b365f0db
Andrew Ho <andrewjiameng.ho@mail.utoronto.ca>
2020-12-11 18:28:40 -0500

    LocationController javadoc

--- phase2/src/controllers/event/LocationController.java
+++ phase2/src/controllers/event/LocationController.java
@@ -6,22 +6,55 @@ import use_cases.event.LocationManager;
 
 import java.util.ArrayList;
 
+/**
+ * A controller responsible for adding and managing locations
+ *
+ * Fields:
+ * LocationManager: stores location information
+ */
+
 public class LocationController {
     private final LocationManager locationManager;
+
+    /**
+     * Creates an instance of <code>LocationController </code> with given parameters.
+     *
+     * @param dm Datamanager containing all needed managers.
+     */
     public LocationController(DataManager dm){
         this.locationManager = dm.getLocationManager();
     }
 
+    /**
+     * @return true iff there is already a location called name
+     * @param name desired name to check
+     */
     public boolean isNewLocation(String name) {
         try { locationManager.checkNewLocation(name); }
         catch (LocationAlreadyExistsException e) { return false; }
         return true;
     }
-    
-    public void addNewLocation(String name, int capacity, int tables, int chairs, boolean hasInternet, boolean hasSoundSystem, boolean hasPresentationScreen, String furtherNotes) {
-        this.locationManager.addNewLocation(name, capacity, tables, chairs, hasInternet, hasSoundSystem, hasPresentationScreen, furtherNotes);
+
+    /**
+     * Add a location based on the information given.
+     * @param name name to add
+     * @param capacity capacity to add
+     * @param tables table count to add
+     * @param chairs chair count to add
+     * @param hasInternet internet existence to add
+     * @param hasSoundSystem sound system existence to add
+     * @param hasPresentationScreen presentation screen existence to add
+     * @param furtherNotes further notes to add
+     */
+    public void addNewLocation(String name, int capacity, int tables, int chairs, boolean hasInternet,
+                               boolean hasSoundSystem, boolean hasPresentationScreen, String furtherNotes) {
+        this.locationManager.addNewLocation(name, capacity, tables, chairs, hasInternet, hasSoundSystem,
+                hasPresentationScreen, furtherNotes);
     }
 
+    /**
+     * @return a list of names of all locations store in the LocationManager
+     */
     public ArrayList<String> getLocationsAsString() {
         return locationManager.getAllLocationsAsString();
     }
