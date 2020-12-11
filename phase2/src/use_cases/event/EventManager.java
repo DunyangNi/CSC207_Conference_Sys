f0e720b8ac3f732d76402cc543390487baa6796b
Andrew Ho <andrewjiameng.ho@mail.utoronto.ca>
2020-12-11 17:22:00 -0500

    EventManager Javadoc

--- phase2/src/use_cases/event/EventManager.java
+++ phase2/src/use_cases/event/EventManager.java
@@ -38,10 +38,19 @@ public class EventManager implements Serializable, HTMLWritable {
     // Methods
     //------------------------------------------------------------
 
+    /**
+     * @return True iff event is full (Attendees fill capacity)
+     * @param id ID of desired event to inspect
+     */
+
     private boolean eventIsFull(Integer id) {
         Event selectedEvent = events.get(id);
         return selectedEvent.getCapacity() == selectedEvent.getAttendees().size();
     }
+
+    /**
+     * @return list of events
+     */
 
     public ArrayList<Event> fetchEventList() {
         return new ArrayList<>(events.values());
@@ -58,12 +67,22 @@ public class EventManager implements Serializable, HTMLWritable {
         return events.get(id).getAttendees();
     }
 
+    /**
+     * @return list of events that a speaker is talking at
+     * @param speaker username of desired speaker
+     */
+
     private ArrayList<Event> getSpeakerEvents(String speaker) {
         ArrayList<Event> speakerTalks = new ArrayList<>();
         EventVisitor visitor = new EventHelper();
         for (Event e : fetchEventList()) { if (e.acceptSpeakers(visitor).contains(speaker)) speakerTalks.add(e); }
         return speakerTalks;
     }
+
+    /**
+     * @return list of sorted events
+     * @param selectedEvents a list of events that we want to sort
+     */
 
     private ArrayList<String> getSortedEvents(ArrayList<Event> selectedEvents) {
         Event[] selectedTalksToSort = selectedEvents.toArray(new Event[0]);
@@ -75,15 +94,49 @@ public class EventManager implements Serializable, HTMLWritable {
         return sortedSelectedEvents;
     }
 
+    /**
+     * @return list of sorted events by event ID (Increasing order)
+     * @param eventIDs list of event IDs which we want to sort
+     */
+
     public ArrayList<String> getSortedEventsByID(ArrayList<Integer> eventIDs) {
         ArrayList<Event> selectedEvents = new ArrayList<>();
         for (Integer id : eventIDs) { selectedEvents.add(events.get(id)); }
         return getSortedEvents(selectedEvents);
     }
 
+    /**
+     * @return list of all events in Eventmanager, now sorted
+     */
+
     public ArrayList<String> getAllSortedEvents() { return getSortedEvents(fetchEventList()); }
 
+    /**
+     * @return sorted list of events where a speaker is talking at
+     * @param speaker username of desired speaker
+     */
+
     public ArrayList<String> getSpeakerSortedEvents(String speaker) { return getSortedEvents(getSpeakerEvents(speaker)); }
+
+    /** Attempts to add a new event with the given information.
+     * @param type an enum representing the type of the desired event
+     * @param topic desired topic
+     * @param time desired time
+     * @param location desired location
+     * @param organizer desired organizer
+     * @param speakers list of desired speakers
+     * @param capacity desired capacity
+     * @param tables desired table count
+     * @param chairs desired chair count
+     * @param hasInternet desired internet requirement
+     * @param hasSoundSystem desired sound system requirement
+     * @param vipOnly desired VIP restriction status
+     *
+     * @throws InvalidEventTypeException when type is an invalid event type
+     * @throws OutOfScheduleException when time is an invalid event time
+     * @throws LocationInUseException when location is already in use at the desired time
+     * @throws SpeakerIsBusyException when speaker is already at another event at the desired time
+     */
 
     public void addNewEvent(EventTypeEnum type,
                             String topic, Calendar time, String location, String organizer,
@@ -109,6 +162,17 @@ public class EventManager implements Serializable, HTMLWritable {
         if (!(Calendar.getInstance().compareTo(talkToCancel.getTime()) >= 0)) events.remove(id);
     }
 
+    /**
+     * Changes the time of an event.
+     *
+     * @param id id of event
+     * @param newTime desired time
+     * @throws OutOfScheduleException when newTime is an invalid time
+     * @throws LocationInUseException when location is already in use at the desired time
+     * @throws SpeakerIsBusyException when speaker is at another talk at desired time
+     * @throws EventNotFoundException when id is not valid
+     */
+
     public void changeTime(Integer id, Calendar newTime) throws OutOfScheduleException, LocationInUseException, SpeakerIsBusyException, EventNotFoundException {
         if (!events.containsKey(id)) throw new EventNotFoundException();
         Event selectedEvent = events.get(id);
@@ -118,9 +182,26 @@ public class EventManager implements Serializable, HTMLWritable {
         eventModifier.ChangeTime(events.get(id), newTime);
     }
 
-    public void checkValidEvent(Calendar time, String location, ArrayList<String> speakers) throws OutOfScheduleException, LocationInUseException, SpeakerIsBusyException {
+    /**
+     * Checks if a hypothetical event given a time and location would be valid (no conflict) with given list
+     * of speakers
+     *
+     * @param time hypothetical time
+     * @param location hypothetical location
+     *
+     * For the throws, check out the method in EventChecker
+     */
+
+    public void checkValidEvent(Calendar time, String location, ArrayList<String> speakers) throws OutOfScheduleException,
+            LocationInUseException, SpeakerIsBusyException {
         eventChecker.checkValidEvent(time, location, speakers, fetchEventList());
     }
+
+    /**
+     * @return True iff speaker is talking at event with ID id
+     * @param id ID of desired event
+     * @param speaker username of desired speaker
+     */
 
     public boolean isSpeakerOfEvent(Integer id, String speaker) {
         EventVisitor visitor = new EventHelper();
@@ -138,6 +219,15 @@ public class EventManager implements Serializable, HTMLWritable {
         return events.get(id).getAttendees().contains(attendee);
     }
 
+    /**
+     * Add an attendee to an event
+     * @param id ID of desired event
+     * @param attendee username of desired attendee
+     * @throws EventIsFullException when event is full
+     * @throws EventNotFoundException when id is invalid
+     * @throws AlreadySignedUpException when attendee already signed up
+     */
+
     public void addAttendee(Integer id, String attendee) throws EventIsFullException, EventNotFoundException, AlreadySignedUpException {
         if (!events.containsKey(id))
             throw new EventNotFoundException();
@@ -148,6 +238,14 @@ public class EventManager implements Serializable, HTMLWritable {
         events.get(id).getAttendees().add(attendee);
     }
 
+    /**
+     * Remove an attendee from an event
+     * @param id ID of desired event
+     * @param attendee username of desired attendee
+     * @throws EventNotFoundException when id is invalid
+     * @throws AttendeeNotFoundException when attendee parameter is invalid
+     */
+
     public void removeAttendee(Integer id, String attendee) throws EventNotFoundException, AttendeeNotFoundException {
         if (!events.containsKey(id))
             throw new EventNotFoundException();
@@ -155,6 +253,11 @@ public class EventManager implements Serializable, HTMLWritable {
             throw new AttendeeNotFoundException();
         events.get(id).getAttendees().remove(attendee);
     }
+
+    /**
+     * @return True iff event is VIP only
+     * @param id ID of desired event
+     */
 
     public boolean getVipRestriction(Integer id){
         return events.get(id).getVipOnly();
