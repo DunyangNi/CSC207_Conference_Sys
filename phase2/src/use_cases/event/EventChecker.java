7ea682064022831e30e3ade021377aa25cbd0c7b
Andrew Ho <andrewjiameng.ho@mail.utoronto.ca>
2020-12-11 17:04:46 -0500

    EventCheck, EventModifier javadoc

--- phase2/src/use_cases/event/EventChecker.java
+++ phase2/src/use_cases/event/EventChecker.java
@@ -6,21 +6,33 @@ import entities.event.Talk;
 import exceptions.OutOfScheduleException;
 import exceptions.conflict.LocationInUseException;
 import exceptions.conflict.SpeakerIsBusyException;
+import exceptions.not_found.AccountNotFoundException;
+import exceptions.not_found.ContactNotFoundException;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 /**
- * Represents a checking logic for events
+ * Represents a checking logic for events. Helper class of EventManager
  */
 public class EventChecker implements Serializable {
 
-    //------------------------------------------------------------
-    // Methods
-    //------------------------------------------------------------
+    /**
+     * Check if the hypothetical event with time and location is valid and does not create conflicts in the given list
+     * of speakers and and events
+     *
+     * @param time Hypothetical event time
+     * @param location Hypothetical event location
+     * @param speakers List of speakers
+     * @param events List of events
+     * @throws OutOfScheduleException if the hypothetical time is not a valid event time
+     * @throws LocationInUseException if the hypothetical time and location would create a double booking of a location
+     * @throws SpeakerIsBusyException if the hypothetical time and location would create a double booking of a speaker
+     */
 
-    public void checkValidEvent(Calendar time, String location, ArrayList<String> speakers, ArrayList<Event> events) throws OutOfScheduleException, LocationInUseException, SpeakerIsBusyException {
+    public void checkValidEvent(Calendar time, String location, ArrayList<String> speakers, ArrayList<Event> events)
+            throws OutOfScheduleException, LocationInUseException, SpeakerIsBusyException {
         if (!(9 <= time.get(Calendar.HOUR_OF_DAY) && time.get(Calendar.HOUR_OF_DAY) <= 16)) throw new OutOfScheduleException();
         for (Event event : events) {
             if (event.getTime().equals(time)) {
