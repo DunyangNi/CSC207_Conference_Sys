2e9ea807b267ada5e454da670d30d6b0d5d405f5
Andrew Ho <andrewjiameng.ho@mail.utoronto.ca>
2020-12-11 17:40:04 -0500

    AccountController javadoc

--- phase2/src/controllers/account/AccountController.java
+++ phase2/src/controllers/account/AccountController.java
@@ -9,17 +9,33 @@ import views.ViewFactory;
 
 import java.util.Set;
 
+/**
+ * A contoller class responsible for getting and dealing with Account types and lists
+ *
+ * Fields:
+ * DataManager: Datamanager class which stores information of accounts and an accountmanager
+ * AccountManager: AccountManager class which stores accounts
+ * username: username of the user using the program
+ */
 public class AccountController {
     private final DataManager dm;
     private final AccountManager am;
     private final String username;
 
+    /**
+     * Creates an instance of <code>AccountController</code> with given parameters..
+     *
+     * @param dm desired Datamanager
+     */
     public AccountController(DataManager dm) {
         this.dm = dm;
         this.am = dm.getAccountManager();
         this.username = dm.getUsername();
     }
 
+    /**
+     * @return enum representing the account type of the account with the username
+     */
     public AccountTypeEnum getAccountType() {
         if (am.containsAttendee(username)) {
             if (am.isVipAttendee(username)) {
@@ -34,19 +50,33 @@ public class AccountController {
         }
     }
 
+    /**
+     * @return true iff the AccountManager contains an account with the given username
+     * @param username desired username
+     */
     public boolean usernameExists(String username) {
         return am.containsAccount(username);
     }
 
+    /**
+     * @return list of accounts in the AccountManager
+     */
     public Set<String> getAccountList() {
         return dm.getAccountManager().getAccountHashMap().keySet();
     }
 
+    /**
+     * @return a view based on the view enum given
+     * @param viewEnum the given view enum
+     */
     public View getView(ViewEnum viewEnum) { // TODO find more appropriate place. ATM, Controller is interacting with View
         ViewFactory viewFactory = new ViewFactory(dm);
         return viewFactory.getView(viewEnum);
     }
 
+    /**
+     * Lets the datamanager save its current data.
+     */
     public void saveData() {
         dm.saveData();
     }
