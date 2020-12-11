8c00e973b06c95a4eb7b6bacfef684cfb7005075
Andrew Ho <andrewjiameng.ho@mail.utoronto.ca>
2020-12-11 16:14:19 -0500

    Revised event javadocs

--- phase2/src/entities/event/Event.java
+++ phase2/src/entities/event/Event.java
@@ -5,7 +5,23 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 /**
- * Represents any event in the system.
+ * Represents an event in the system.
+ *
+ * Fields:
+ * topic: the topic of the event
+ * location: the location of the event
+ * time: the time of the event
+ * organizer: the organizer of the event
+ * vipOnly: represents whether or not the event is restricted to VIP attendee accounts
+ * capacity: the maximum allowed attendees to sign up to this event
+ * tables: the tables used in the event
+ * chairs: the chairs used in the event
+ * requiresInternet: represents if internet is required in the event
+ * requiresSoundSystem: represents if sound system is required
+ * requiresPresentationScreen: represents if a presentation screen is required
+ *
+ * attendees: list of usernames of attendees attending the event
+ * id: the ID identifer of the event and cannot be changed
  */
 public class Event implements Serializable, Comparable<Event>, EventAcceptor {
     private String topic;
@@ -23,7 +39,11 @@ public class Event implements Serializable, Comparable<Event>, EventAcceptor {
     private final ArrayList<String> attendees = new ArrayList<>();
     private final int id;
 
-    public Event(Integer id, String topic, Calendar time, String location, String organizer, int capacity, int tables, int chairs, boolean requiresInternet, boolean requiresSoundSystem, boolean requiresPresentationScreen, Boolean vipOnly) {
+
+
+    public Event(Integer id, String topic, Calendar time, String location, String organizer,
+                 int capacity, int tables, int chairs, boolean requiresInternet, boolean requiresSoundSystem,
+                 boolean requiresPresentationScreen, Boolean vipOnly) {
         this.topic = topic;
         this.time = time;
         this.location = location;
@@ -43,7 +63,8 @@ public class Event implements Serializable, Comparable<Event>, EventAcceptor {
      * Used to sort Array of <code>Event</code> by time.
      *
      * @param event given <code>Event</code>
-     * @return 0 iff times are the same; positive iff this event is after the given; negative iff this event is before the given
+     * @return 0 iff times are the same; positive iff this event is after the given;
+     * negative iff this event is before the given
      */
     public int compareTo(Event event) {
         return this.time.compareTo(event.time);
@@ -76,6 +97,9 @@ public class Event implements Serializable, Comparable<Event>, EventAcceptor {
         return topic.hashCode() / 10 + getOrganizer().hashCode() % 1000;
     }
 
+    /**
+     * @return a string representation of the event
+     */
     @Override
     public String toString() {
         return "Event ID: " + this.id + "\n" +
@@ -85,6 +109,10 @@ public class Event implements Serializable, Comparable<Event>, EventAcceptor {
                 "Time: " + this.time.getTime().toString() + "\n" +
                 "Organizer: " + this.organizer;
     }
+
+    //------------------------------------------------------------
+    // Getters
+    //------------------------------------------------------------
 
     /**
      * @return topic of this <code>Event</code>
@@ -126,9 +154,54 @@ public class Event implements Serializable, Comparable<Event>, EventAcceptor {
      */
     public ArrayList<String> getAttendees() { return attendees; }
 
+    /**
+     * @return VIP restriction status of the event
+     */
     public Boolean getVipOnly() { return vipOnly; }
 
+    /**
+     * @return capacity of event
+     */
     public Integer getCapacity() { return capacity; }
+
+    /**
+     * @return number of tables
+     */
+    public Integer getTables(){
+        return this.tables;
+    }
+
+    /**
+     * @return number chairs
+     */
+    public Integer getChairs(){
+        return this.chairs;
+    }
+
+    /**
+     * @return internet requirement
+     */
+    public Boolean getRequiresInternet(){
+        return this.requiresInternet;
+    }
+
+    /**
+     * @return sound system requirement
+     */
+    public Boolean getRequiresSoundSystem(){
+        return this.requiresSoundSystem;
+    }
+
+    /**
+     * @return presentation screen requirement
+     */
+    public Boolean getRequiresPresentationScreen(){
+        return this.requiresPresentationScreen;
+    }
+
+    //------------------------------------------------------------
+    // Setters
+    //------------------------------------------------------------
 
     /**
      * Sets a new topic for this <code>Event</code>.
@@ -148,7 +221,11 @@ public class Event implements Serializable, Comparable<Event>, EventAcceptor {
         this.time = time;
     }
 
-
+    /**
+     * Sets a new location string for this <code>Event</code>.
+     *
+     * @param location new location string for <code>Event</code>
+     */
     public void setLocation(String location) {
         this.location = location;
     }
@@ -162,46 +239,61 @@ public class Event implements Serializable, Comparable<Event>, EventAcceptor {
         this.organizer = organizer;
     }
 
+    /**
+     * Sets a new VIP restriction status for this <code>Event</code>.
+     *
+     * @param newBoolean new VIP restriction for this <code>Event</code>
+     */
     public void setVipOnly(Boolean newBoolean) { this.vipOnly = newBoolean; }
 
+    /**
+     * Sets a capacity for this <code>Event</code>.
+     *
+     * @param newCapacity new capacityfor this <code>Event</code>
+     */
     public void setCapacity(Integer newCapacity) { this.capacity = newCapacity; }
 
-    public Integer getTables(){
-        return this.tables;
-    }
-
-    public Integer getChairs(){
-        return this.chairs;
-    }
-
-    public Boolean getRequiresInternet(){
-        return this.requiresInternet;
-    }
-
-    public Boolean getRequiresSoundSystem(){
-        return this.requiresSoundSystem;
-    }
-
-    public Boolean getRequiresPresentationScreen(){
-        return this.requiresPresentationScreen;
-    }
-
+    /**
+     * Sets number of tables for this <code>Event</code>.
+     *
+     * @param newTables new number of tables for this <code>Event</code>
+     */
     public void setTables(int newTables) {
         this.tables = newTables;
     }
 
+    /**
+     * Sets number of chairs for this <code>Event</code>.
+     *
+     * @param newChairs new number of chairs for this <code>Event</code>
+     */
     public void setChairs(int newChairs) {
         this.chairs = newChairs;
     }
 
+    /**
+     * Sets internet requirement for this <code>Event</code>.
+     *
+     * @param newHasInternet new internet requirement for this <code>Event</code>
+     */
     public void setRequiresInternet(boolean newHasInternet) {
         this.requiresInternet = newHasInternet;
     }
 
+    /**
+     * Sets sound system requirement for this <code>Event</code>.
+     *
+     * @param newHasSoundSystem new sound system requirement for this <code>Event</code>
+     */
     public void setRequiresSoundSystem(boolean newHasSoundSystem) {
         this.requiresSoundSystem = newHasSoundSystem;
     }
 
+    /**
+     * Sets presentation screen requirement for this <code>Event</code>.
+     *
+     * @param newHasPresentationScreen new presentation screen requirement for this <code>Event</code>
+     */
     public void setRequiresPresentationScreen(boolean newHasPresentationScreen) {
         this.requiresPresentationScreen = newHasPresentationScreen;
     }
