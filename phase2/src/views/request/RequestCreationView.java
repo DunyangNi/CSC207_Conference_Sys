e5b127d782ebc5ab0a529482eb536d4b39c29ae3
Jerry Bao <bao.jerry.can@gmail.com>
2020-12-10 22:01:20 -0500

    hi

--- phase2/src/views/request/RequestCreationView.java
+++ phase2/src/views/request/RequestCreationView.java
@@ -1,4 +1,31 @@
 package views.request;
+import enums.ViewEnum;
+import presenters.request.RequestSendPresenter;
+import views.*;
+import controllers.request.*;
+import java.util.Scanner;
 
-public class RequestCreationView {
+public class RequestCreationView implements View{
+    public final RequestController rc;
+    public final RequestSendPresenter rsp;
+    private final Scanner userInput = new Scanner(System.in);
+
+    public RequestCreationView(RequestController rc, RequestSendPresenter rsp) {
+        this.rc = rc;
+        this.rsp = rsp;
+    }
+
+    public ViewEnum runView(){
+        rsp.startPrompt();
+        rsp.subjectLinePrompt();
+
+        String subjectLine = userInput.nextLine();
+
+        rsp.requestContentPrompt();
+
+        String content = userInput.nextLine();
+
+        rsp.exitPrompt();
+        return ViewEnum.VOID;
+    }
 }
