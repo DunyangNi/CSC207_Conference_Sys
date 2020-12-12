069b9b2512d23c902986a243d696f073c87e67d6
Andrew Ho <andrewjiameng.ho@mail.utoronto.ca>
2020-12-11 19:57:59 -0500

    Gateway javadoc

--- phase2/src/gateways/ContactDataManager.java
+++ phase2/src/gateways/ContactDataManager.java
@@ -4,20 +4,36 @@ import java.io.*;
 
 import use_cases.account.ContactManager;
 
+/**
+ * Reads and saves contact data and creates ContactManager
+ * Fields:
+ * contactPath: Path of file storing contact data
+ */
 public class ContactDataManager implements DataReader, DataSaver {
-    private final String friendPath;
+    private final String contactPath;
 
+    /**
+     * Empty constructor. Sets path as ContactManager
+     */
     public ContactDataManager() {
         this("ContactManager");
     }
 
-    public ContactDataManager(String friendPath) {
-        this.friendPath = friendPath;
+    /**
+     * Non-empty constructor. Sets path.
+     * @param contactPath given path
+     */
+    public ContactDataManager(String contactPath) {
+        this.contactPath = contactPath;
     }
 
+    /**
+     * From the path, attempt to read and create a ContactManager.
+     * Create a new ContactManager if reading does not succeed.
+     */
     public ContactManager readManager() {
         try{
-            return (ContactManager) readObject(friendPath);
+            return (ContactManager) readObject(contactPath);
         } catch (IOException e) {
             System.out.println("Could not read ContactManager, creating a new ContactManager.");
             return new ContactManager();
